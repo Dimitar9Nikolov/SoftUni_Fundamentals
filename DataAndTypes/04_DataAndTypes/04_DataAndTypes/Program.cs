729272aabe3002c99fc0de274f1a23929cde7ddb
Dimitar N <mitko.rnikolov@gmail.com>
2025-01-19 18:57:20 +0200

    04_DataAndTypes

---
+++ DataAndTypes/04_DataAndTypes/04_DataAndTypes/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _04_DataAndTypes;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numberOfLines = int.Parse(Console.ReadLine());
+        int sum = 0;
+        while (numberOfLines > 0)
+        {
+            char letter = char.Parse(Console.ReadLine());
+            numberOfLines--;
+            int ascii = (int)letter;
+            sum += ascii;
+        }
+        Console.WriteLine(sum);
+    }
+}
+
