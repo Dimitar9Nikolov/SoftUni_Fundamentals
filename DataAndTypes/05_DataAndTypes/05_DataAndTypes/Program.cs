f46c9e791c32feac5bd35ae58c4be94ba00c0fa4
Dimitar N <mitko.rnikolov@gmail.com>
2025-01-19 13:15:10 +0200

    05_DataAndTypes_Done

---
+++ DataAndTypes/05_DataAndTypes/05_DataAndTypes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace _05_DataAndTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startNumber = int.Parse(Console.ReadLine());
+            int endNumber = int.Parse(Console.ReadLine());
+            for (int i = startNumber; i <= endNumber; i++)
+            {
+                char ASCII = (char)i;
+                Console.Write($"{ASCII} ");
+            }
+
+
+            char asciiValue = (char)startNumber;
+        }
+    }
+}
